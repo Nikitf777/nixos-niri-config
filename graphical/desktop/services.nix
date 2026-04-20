7eb733f680d53f0d9f9c00bd4a3c5d9885c5baac
Nikita Samusev <nikitf777@gmail.com>
2026-04-20 14:19:07 +0300

    Remove xserver

--- graphical/desktop/services.nix
+++ graphical/desktop/services.nix
@@ -4,13 +4,6 @@
   services.displayManager.sddm.enable = true;
   services.displayManager.sessionPackages = with pkgs; [ niri ];
   services.desktopManager.gnome.enable = true;
-  services.xserver = {
-    enable = true;
-    xkb = {
-      layout = "us,ru";
-      variant = "";
-    };
-  };
   # Workaround for niri not supporting using the Super key as a standalone bind (and doesn't seem to support the F keys above F12 unfortunately)
   services.keyd = {
     enable = true;
