1f2da1c7e26fe8bf29345328a03d2a74ecbd4f41
Nikita Samusev <nikitf777@gmail.com>
2026-04-29 20:09:00 +0300

    Define services options as a block

--- graphical/services.nix
+++ graphical/services.nix
@@ -1,11 +1,13 @@
 { config, pkgs, ... }:
 {
   # Enable sound with pipewire.
-  services.pulseaudio.enable = false;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
+  services = {
+    pulseaudio.enable = false;
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+    };
   };
 }
