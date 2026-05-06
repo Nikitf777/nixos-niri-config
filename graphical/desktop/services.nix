dd6b3d4458b18e043da5c787973801226f25f99a
Nikita Samusev <nikitf777@gmail.com>
2026-05-07 01:39:37 +0300

    Define services in a block for the Desktop Specialisation

--- graphical/desktop/services.nix
+++ graphical/desktop/services.nix
@@ -1,41 +1,43 @@
 { config, pkgs, ... }:
 {
   # Desktop
-  services.displayManager.sddm.enable = true;
-  services.displayManager.sessionPackages = with pkgs; [ niri ];
-  # Workaround for niri not supporting using the Super key as a standalone bind (and doesn't seem to support the F keys above F12 unfortunately)
-  services.keyd = {
-    enable = true;
-    keyboards = {
-      default = {
-        ids = [ "*" ];
-        settings = {
-          main = {
-            meta = "overload(meta, scrolllock)";
+  services = {
+    displayManager.sddm.enable = true;
+    displayManager.sessionPackages = with pkgs; [ niri ];
+    # Workaround for niri not supporting using the Super key as a standalone bind (and doesn't seem to support the F keys above F12 unfortunately)
+    keyd = {
+      enable = true;
+      keyboards = {
+        default = {
+          ids = [ "*" ];
+          settings = {
+            main = {
+              meta = "overload(meta, scrolllock)";
+            };
           };
         };
       };
     };
-  };
 
-  services.flatpak = {
-    enable = true;
-    remotes = [
-      {
-        name = "flathub";
-        location = "https://dl.flathub.org/repo/flathub.flatpakrepo";
-      }
-      {
-        name = "flathub-beta";
-        location = "https://flathub.org/beta-repo/flathub-beta.flatpakrepo";
-      }
-      {
-        name = "gnome-nightly";
-        location = "https://nightly.gnome.org/gnome-nightly.flatpakrepo";
-      }
-    ];
-  };
+    flatpak = {
+      enable = true;
+      remotes = [
+        {
+          name = "flathub";
+          location = "https://dl.flathub.org/repo/flathub.flatpakrepo";
+        }
+        {
+          name = "flathub-beta";
+          location = "https://flathub.org/beta-repo/flathub-beta.flatpakrepo";
+        }
+        {
+          name = "gnome-nightly";
+          location = "https://nightly.gnome.org/gnome-nightly.flatpakrepo";
+        }
+      ];
+    };
 
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
+    # Enable CUPS to print documents.
+    printing.enable = true;
+  };
 }
